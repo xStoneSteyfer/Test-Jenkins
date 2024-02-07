68b1070f2ed67dee22a70f99f1e30bfb802a9f13
VAN CAUWENBERGE Xavier <xavier.vancau@nordnet.fr>
2024-02-07 11:32:41 +0100

    modif

--- Test.py
+++ Test.py
@@ -8,15 +8,4 @@ class TestSum(unittest.TestCase):
     def test_sum_incorrect(self):
         self.assertEqual(main.sum(5, 5), 9, "Erreur: fonction sum")
 
-if __name__ == '__main__':
-    unittest.main()
-
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestSum))
-    result = unittest.TestResult()
-    suite.run(result)
-
-    if len(result.failures) > 0:
-        print("Liste des tests échoués :")
-        for test, traceback in result.failures:
-            print(test)
+unittest.main()
