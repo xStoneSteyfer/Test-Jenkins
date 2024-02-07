75af94277af46f46a2f2bda2583f32a70d8dcd99
VAN CAUWENBERGE Xavier <xavier.vancau@nordnet.fr>
2024-02-07 11:01:30 +0100

    modif

--- Test.py
+++ Test.py
@@ -5,9 +5,6 @@ import main
 class TestSum(unittest.TestCase):
     def testsum(self):
         self.assertEqual(main.sum(5, 5), 10, "Erreur: fonction sum")
-        self.assertEqual(main.sum(4, 5), 2, "Erreur: fonction sum")
-        self.assertEqual(main.sum(51, -50), 1, "Erreur: fonction sum")
-        self.assertEqual(main.sum(1, 8), 7, "Erreur: fonction sum")
 
 
 unittest.main()
